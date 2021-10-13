33f53167cb706366a1657ed94d2ca1e146c39953
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-14 01:19:51 +0300

    promises adding

---
+++ promiseExercises/01-making-promises/answers.js
@@ -0,0 +1,76 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-console */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+
+/**
+ * 
+ * EXERCISE 1
+ * 
+ * @returns {Promise<3>}
+ */
+function makePromiseResolveWith3(){
+  /* IMPLEMENT ME! */
+  return new Promise((resolve)=>{
+    resolve(3)
+  })
+}
+// makePromiseResolveWith3().then((dip)=>console.log(dip))
+/**
+ * 
+ * EXERCISE 2
+ * 
+ * @returns {Promise<,"Boo!">}
+ */
+function makePromiseRejectWithBoo(){
+  /* IMPLEMENT ME! */
+  return new Promise((resolve, reject)=>{
+    reject('Boo!')
+  })
+}
+
+/**
+ * 
+ * EXERCISE 3
+ * 
+ * @param {boolean} itShouldResolve - Whether the promise should resolve or reject
+ * @returns {Promise<undefined, undefined>}
+ */
+
+function makePromiseWithConstructor(itShouldResolve){
+  return new Promise((resolve, reject) => {
+    /* If itShouldResolve is true, call resolve */
+    /* If itShouldResolve is false, call reject */
+    if (itShouldResolve) {
+      resolve()
+    } else {
+      reject()
+    }
+  });
+}
+
+/**
+ * 
+ * EXERCISE 4
+ *
+ * @param {any} value 
+ * @param {number} delayInMs 
+ * @return {Promise<any>} - A promise that will resolve with the value after delayInMs milliseconds
+ */
+function makeDelayPromise(value, delayInMs){
+  /* Return a promise that resolves with the value after delayInMs */
+  return new Promise(resolve => {
+    setTimeout(()=>{
+      resolve(value)
+    }, delayInMs)
+  });
+}
+
+module.exports = {
+  makePromiseResolveWith3,
+  makePromiseRejectWithBoo,
+  makePromiseWithConstructor,
+  makeDelayPromise,
+};
