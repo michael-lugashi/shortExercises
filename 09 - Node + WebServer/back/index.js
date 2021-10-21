9ae351f27a226f8980eb66569695e8861e005183
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-10-21 08:45:59 +0300

    Finish api Request and server

--- 09 - Node + WebServer/back/index.js
+++ 09 - Node + WebServer/back/index.js
@@ -1,32 +1,59 @@
 'use strict';
 const http = require('http');
+const data = require('./db');
 
 const server = http.createServer((req, res) => {
   let body = '';
   // header
-  console.dir(req.param);
+  //   console.dir(req.param);
   // get main route
+  //   if (req.method === 'GET') {
+  //     console.log('help');
+  //     res.writeHead(200, 'ok', {
+  //       'Content-Type': 'application/json',
+  //       'Access-Control-Allow-Origin': '*',
+  //     });
+  //     res.write(`{name: 'daniel'}`);
+  //     res.end();
+  //   }
   if (req.method === 'POST') {
     //   res.write("hey");
-    console.log('hey');
-
+    // console.log('hey');
     // when there is data
     req.on('data', (data) => {
-      console.log(data);
+      // console.log(JSON.stringify(data));
       body += data;
+      //   res.write(data);
+
+      //   res.end()
     });
-    // end of req
+
+    // // end of req
     req.on('end', () => {
       console.log('Body: ' + body);
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.end('post recieved');
+      res.writeHead(200, 'ok', {
+        'Content-Type': 'text/html',
+        'Access-Control-Allow-Origin': '*',
+      });
+      let validated = true;
+      const info = JSON.parse(body);
+      const { name, age, abilityApp } = info;
+      const { nameNotEqual, minAge, maxAge, ability } = data.validStudent;
+      console.log(abilityApp);
+      console.log(ability);
+      if (nameNotEqual.includes(name)) {
+        validated = false;
+      }
+      if (age < minAge || age > maxAge) {
+        validated = false;
+      }
+      if (!ability.includes(abilityApp)) {
+        validated = false;
+      }
+      res.end(String(validated));
     });
   }
 });
 server.listen(8080, () => {
   console.log('listening to 8080');
 });
-//   server.on('connection', (socket)=> {
-//     console.log('new connection')
-// })
-//   && req.url === "/"
--- 09 - Node + WebServer/front/index.js
+++ 09 - Node + WebServer/front/index.js
@@ -2,20 +2,34 @@
 form.addEventListener('submit', submitted);
 async function submitted(event) {
   event.preventDefault();
-    console.log(nameApp.value)
-  console.log(event);
+  //     console.log(nameApp.value)
+  //   console.log(event);
+  //   axios.get('http://localhost:8080/').then(res=>{
+  //       console.log(res)
+  //   })
+  let applicantInfo = {
+    name: nameApp.value,
+    age: age.value,
+    abilityApp: ability.value,
+  };
   axios
-    .post('..//back//index.js', )
+    .post('http://localhost:8080/', JSON.stringify(applicantInfo))
     .then((res) => {
       console.log(res);
+      if (!res.data){
+          alert('You were rejected')
+      }
+      if (res.data) {
+          alert('Congrats youve been accepted')
+      }
     })
     .catch((err) => {
       console.log(err);
     });
-// try {
-//     const data = await axios.get("http://localhost:8080/");
-//     console.log(data);
-//   } catch (error) {
-//     console.log(error);
-//   }
+  // try {
+  //     const data = await axios.get("http://localhost:8080/");
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
 }
